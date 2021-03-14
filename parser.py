3413fa13444707291ddc054bd6a9b3687f9aa29f
Konstantin <balashov-92@mail.ru>
2021-03-14 22:06:24 +0300

    separete PhoneParser to two classes PageLoader and PageParser. added lock to max threads

--- parser.py
+++ parser.py
@@ -1,41 +1,78 @@
-import threading
 import time
+import re
+import threading
 
 import requests
-import re
 
+
+MAX_THREADS_LOCKER = threading.BoundedSemaphore(100)
 PHONE_TEMPLATE = r'[7|8]?[-|\s]?\(?\d{3}?\)?[-|\s]?\d{3}[-|\s]?\d{2}[-|\s]?\d{2}<'
+
 links = [
     'https://masterdel.ru/',
     'https://repetitors.info/',
+
 ]
 
 
-class PhoneParser(threading.Thread):
+class PageLoader(threading.Thread):
+    """Class Page Parser.
+    Load html pages from given url
+    """
+
+    def __init__(self, url, pages, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.url = url
+        self.pages = pages
+
+    def run(self):
+        MAX_THREADS_LOCKER.acquire()
+        try:
+            response = requests.get(self.url)
+            self.pages.append(response.text)
+            with open('processed.txt', 'a', encoding='utf8') as file:
+                file.write(f'{time.ctime(time.time())}   '
+                           f'SUCCESS: {self.url} \n')
+        except (requests.exceptions.MissingSchema,
+                requests.exceptions.InvalidSchema,
+                requests.exceptions.InvalidURL,
+                requests.exceptions.ConnectionError,
+                requests.exceptions.ReadTimeout,
+                ) as exc:
+            with open('errors.txt', 'a', encoding='utf8') as file:
+                file.write(f'{time.ctime(time.time())} \n'
+                           f'{exc} \n')
+        except Exception as exc:
+            with open('other_errors.txt', 'a', encoding='utf8') as file:
+                file.write(f'{time.ctime(time.time())} \n'
+                           f'{exc} \n')
+        finally:
+            MAX_THREADS_LOCKER.release()
+
+
+class PageParser(threading.Thread):
     """Class Phone Parser.
     Constructor takes url, regular expression template
     and list to store results phone numbers
     """
 
-    def __init__(self, url, phone_template, out_phones, *args, **kwargs):
+    def __init__(self, page, phone_template, out_phones, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.url = url
+        self.page = page
         self.phone_template = phone_template
         self.out_phones = out_phones
 
     def run(self):
         """Parse phones from response and """
+        MAX_THREADS_LOCKER.acquire()
         try:
-            response = requests.get(self.url)
-            input_phones = re.findall(self.phone_template, response.text)
+            input_phones = re.findall(self.phone_template, self.page)
             self.out_phones.extend(self.normalize_phones(input_phones))
-            with open('processed.txt', 'a', encoding='utf8') as file:
-                file.write(f'{time.ctime(time.time())}   '
-                           f'SUCCESS: {self.url} \n')
         except Exception as exc:
             with open('errors.txt', 'a', encoding='utf8') as file:
-                file.write(f'{time.ctime(time.time())} \n'
-                           f'{exc} \n')
+                print(exc)
+        finally:
+            MAX_THREADS_LOCKER.release()
 
     @staticmethod
     def normalize_phones(phones):
@@ -67,18 +104,29 @@ def time_track(func):
     return surrogate
 
 
+def get_pages(urls):
+    loaded_pages = []
+    loaders = [PageLoader(url, loaded_pages) for url in urls]
+    for loader in loaders:
+        loader.start()
+    for loader in loaders:
+        loader.join()
+
+    return loaded_pages
+
+
 @time_track
 def get_phones(urls):
-    phones = []
-    workers = [PhoneParser(url, PHONE_TEMPLATE, phones) for url in urls]
+    out_phones = []
+    parsers = [PageParser(page, PHONE_TEMPLATE, out_phones) for page in
+               get_pages(urls)]
 
-    for worker in workers:
-        worker.start()
+    for parser in parsers:
+        parser.start()
+    for parser in parsers:
+        parser.join()
 
-    for worker in workers:
-        worker.join()
-
-    return phones
+    return out_phones
 
 
 if __name__ == '__main__':
